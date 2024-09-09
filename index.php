e55343ce5ae90186398e12b3e1e5cd4c4fe89c2b
Gabriel FV <gabrielvasco906@gmail.com>
2024-09-09 17:05:25 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        Hello there
+    </h1>
+
+    <?php 
+        const NOME = "denis";
+        $idade = 23;
+        $altura = 1.75;
+        $casado = false;
+
+        echo "acabando com a maldição XD";
+
+        echo "<p> brincando com tipos no php </p>";
+
+        print "Temos o nome $NOME, com a idade de $idade, com a altura de $altura e o estado civil é $casado";
+    ?>
+</body>
+
+</html>
